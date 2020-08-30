41b69c6a0f729cc6b0757274e3509d661a384a21
Julliann Dulay <jullianndulay@gmail.com>
2020-08-30 16:24:27 -0700

    fixed function calls and minor bugs

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -20,32 +20,55 @@ const showSuccess = (input) => {
 }
 
 //check Email is valid
-const isValidEmail(email){
+const checkEmail = (input) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    }else{
+        showError(input, 'Email is not valid')
+    }
 }
 
 //Get FieldName capitalizes first letter
 const getFieldName = (input) => {
-    input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);    
 }
 
 //Check required fields
 const checkRequired = (inputArray) => {
     inputArray.forEach((input)=> {
-        if(input.value.trim() === ''){
-            showError(input, `${getFieldName(input)} is Required!`);            
+        if(input.value.trim() === ''){        
+            showError(input, `${getFieldName(input)} is Required!`)          
         }else{
             showSuccess(input)
         }
     })
 }
 
+//check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be at less than ${max} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+checkPassword = (input1,input2) => {
+    if(input1.value !== input2.value){
+        showError(input2, "Passwords Do not Match")
+    }
+}
 
 
 //Event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault()    
-    checkRequired([form,username,email,password,password2])
+    checkRequired([username,email,password,password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPassword(password,password2);    
 })
