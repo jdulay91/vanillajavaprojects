718a2f6ebde8ef1903957f5298ab5c8f87edadef
Julliann Dulay <jullianndulay@gmail.com>
2020-08-30 20:34:41 -0700

    hooked up some java script for event listening and rendering on the dom

--- MovieSeatBooking/index.js
+++ MovieSeatBooking/index.js
@@ -1,2 +1,33 @@
 const container = document.querySelector('.container')
-const seats = document.querySelectorAll('.seat')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.querySelector('#count')
+const total = document.querySelector('#total')
+const movieSelect = document.querySelector('#movie')
+let ticketPrice = +movieSelect.value
+
+//Update total and count
+const updateSelectedCount = () => {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    const selectedSeatsCount = selectedSeats.length;  
+    
+
+    count.innerText = selectedSeatsCount
+    total.innerText = selectedSeatsCount * ticketPrice
+}
+
+//Movie Select Event
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value
+    updateSelectedCount();
+})
+
+//Seat click event
+container.addEventListener('click', (e) => {
+    if(
+        e.target.classList.contains('seat') && 
+        !e.target.classList.contains('occupied'
+    )) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+})
