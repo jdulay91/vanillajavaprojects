7ea27fa670b66a238b6fa1fe1ec593d0a87f6db0
Julliann Dulay <jullianndulay@gmail.com>
2020-08-30 20:42:34 -0700

    implemented local storage

--- MovieSeatBooking/index.js
+++ MovieSeatBooking/index.js
@@ -5,11 +5,19 @@ const total = document.querySelector('#total')
 const movieSelect = document.querySelector('#movie')
 let ticketPrice = +movieSelect.value
 
+
+//Save selected movie index and price
+const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice',moviePrice)
+}
 //Update total and count
 const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
-    const selectedSeatsCount = selectedSeats.length;  
+    const selectedSeatsCount = selectedSeats.length;    
     
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
 
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice
@@ -18,6 +26,7 @@ const updateSelectedCount = () => {
 //Movie Select Event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value
+    setMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount();
 })
 
