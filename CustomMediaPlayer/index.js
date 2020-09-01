8aa584fa5eba3178a32553728bf414b46f4324b0
Julliann Dulay <jullianndulay@gmail.com>
2020-09-01 09:03:22 -0700

    finished custom media player will make adjustments

--- CustomMediaPlayer/index.js
+++ CustomMediaPlayer/index.js
@@ -24,12 +24,26 @@ const updatePlayIcon = () => {
 
 //update progress and time stamp
 const updateProgress = () => {
-    return true;
+    progress.value = (video.currentTime/ video.duration) * 100;
+
+    //get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10){
+        mins = '0' + String(mins);
+    }
+
+    //get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10) {
+       secs = '0' + String(secs); 
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
-//Sets video progress to corresponding video time
+//Sets video progress to corresponding video time when you click the bar
 const setVideoProgress = () => {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100
 }
 
 //Stops video
