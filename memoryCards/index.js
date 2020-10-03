3fdd2c3223960afa453bc681e02c33aacc71cdf7
Julliann Dulay <jullianndulay@gmail.com>
2020-10-03 12:57:24 -0700

    memory card project added

---
+++ memoryCards/index.js
@@ -0,0 +1,138 @@
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
+
+// keep track of current card
+let currentActiveCard = 0;
+
+//Store DOM cards
+const cardsEl = [];
+
+//Store card data
+const cardsData = getCardsData()
+// const cardsData = [
+// 	{
+// 		question: 'dog',
+// 		answer: 'dog'
+// 	},
+// 	{
+// 		question: 'cat',
+// 		answer: 'cat'
+// 	}
+// ];
+
+//create all cards
+function createCards() {
+	cardsData.forEach((data, index) => createCard(data, index));
+}
+
+//create single card in DOM
+function createCard(data, index) {
+	const card = document.createElement('div');
+	card.classList.add('card');
+
+	if (index === 0) {
+		card.classList.add('active');
+	}
+
+	card.innerHTML = `
+    <div class="inner-card">
+        <div class="inner-card-front">
+        <p>
+         ${data.question}
+        </p>
+        </div>
+         <div class="inner-card-back">
+        <p>
+         ${data.answer}
+        </p>
+        </div>
+    </div>`;
+
+	card.addEventListener('click', () => card.classList.toggle('show-answer'));
+
+	//add to dom cards
+	cardsEl.push(card);
+	cardsContainer.appendChild(card);
+
+	updateCurrentText();
+}
+
+function updateCurrentText() {
+	currentEl.innerText = `${currentActiveCard + 1} /${cardsEl.length}`;
+}
+
+//get cards from local storage
+function getCardsData(){
+    const cards = JSON.parse(localStorage.getItem('cards'))
+    return cards === null ? [] : cards;
+}
+
+function setCardsData(cards){
+    localStorage.setItem('cards',JSON.stringify(cards));
+    window.location.reload()
+}
+
+createCards();
+
+//events
+nextBtn.addEventListener('click', () => {
+	cardsEl[currentActiveCard].className = 'card left';
+
+	currentActiveCard = currentActiveCard + 1;
+
+	if (currentActiveCard > cardsEl.length - 1) {
+		currentActiveCard = cardsEl.length - 1;
+	}
+
+    cardsEl[currentActiveCard].className = 'card active';
+    updateCurrentText()
+});
+prevBtn.addEventListener('click', () => {
+	cardsEl[currentActiveCard].className = 'card right';
+
+	currentActiveCard = currentActiveCard - 1;
+
+	if (currentActiveCard < 0) {
+		currentActiveCard = 0;
+	}
+
+    cardsEl[currentActiveCard].className = 'card active';
+    updateCurrentText()
+});
+
+showBtn.addEventListener('click',()=> addContainer.classList.add('show'))
+hideBtn.addEventListener('click',()=> addContainer.classList.remove('show'))
+
+addCardBtn.addEventListener('click', ()=>{
+
+    const question = questionEl.value;
+    const answer = answerEl.value;
+
+    if(question.trim() && answer.trim()){
+        const newCard = {question, answer}
+
+        createCard(newCard);
+
+        questionEl.value = '';
+        answerEl.value = '';
+
+        addContainer.classList.remove('show');
+        cardsData.push(newCard);
+
+        setCardsData(cardsData)
+    }
+})
+clearBtn.addEventListener('click', ()=>{
+    localStorage.clear();
+    cardsContainer.innerHTML = '';
+    window.location.reload();
+})
